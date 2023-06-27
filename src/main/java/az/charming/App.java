2045d80384e08d37a6c7d0c57c055eb0ce9bc1e1
ShamsMahmudlu <shams.mahmudlu@mail.ru>
2023-06-28 00:55:56 +0400

    added

--- src/main/java/az/charming/App.java
+++ src/main/java/az/charming/App.java
@@ -4,12 +4,10 @@ import az.charming.connect.MySQLConnect;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception {
-
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("connect");
         MySQLConnect.connect();
     }
 }
